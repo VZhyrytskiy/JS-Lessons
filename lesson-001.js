ded17a65773f3d33153305a8ba0412ce23caebb0
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-10-22 19:23:12 +0300

    lesson1 home1

---
+++ lesson-001.js
@@ -0,0 +1,38 @@
+console.log('Привет');
+
+let myNumber = 10.7;
+
+let age = 'vndfjgldfjl';
+
+let t = true;
+let f = false;
+
+let $ddd$ = 80;
+let _ = 90;
+
+typeof t;
+console.log(typeof t);
+console.log(typeof age);
+console.log(typeof myNumber);
+/**
+ * 
+ */
+myNumber = 'fgljfgl';
+//// console.log(myNumber);
+
+
+let city = `Kiev`;
+console.log(city);
+let bestCity = `Odessa`;
+console.log(bestCity);
+city = `lks`;
+console.log(city);
+let name = `Danil`;
+console.log(name);
+name = `Dima`;
+
+let w = 1 + 2 * 3;
+console.log(w);
+let a = 10;
+a += 5;
+console.log(a=='15');
