ff0afe396fbf5c678afe68a8cc6ce10c7551fb81
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-10-31 19:02:41 +0200

    lesson 3

---
+++ Lesson-003/lesson-003.js
@@ -0,0 +1,20 @@
+/**
+ * Тема 3.
+ *  1. Булевые операторы. 
+ *  2. Таблицы истиности
+ */
+
+//  ! - not
+//  && - and 
+//  || - or
+
+// !True == False
+// !False == True
+
+//  &&   True  False
+// True  True  False
+// False False False
+
+//  ||   True  False
+// True  True  True
+// False True  False
