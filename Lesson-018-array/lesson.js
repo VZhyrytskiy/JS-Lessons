3c04ffea195b18890e59a095b063e72f04ca8307
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2019-01-24 21:59:12 +0200

    add lesson 18

---
+++ Lesson-018-array/lesson.js
@@ -0,0 +1,17 @@
+/**
+ * Повтор методів forEach, filter
+ * Прочитати про методи find, findIndex. Подивитись приклади, попробувати запустити їх.
+ * Повторити всі методи масивів, прочитати про них тут https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * - split
+ * - join
+ * - push
+ * - pop
+ * - unshift
+ * - shift
+ * - includes
+ * - reverse
+ * - splice
+ * - indexOf
+ * - lastIndexOf
+ * - slice
+*/
---
+++ Lesson-018-array/tasks.js
@@ -0,0 +1,19 @@
+/**
+ * Розібрати приклади.
+ * https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array
+ * https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/length
+ * https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ * https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+ * https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+ */
